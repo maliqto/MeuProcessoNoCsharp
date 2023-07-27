e6cb5112bf8d457bac871b7f8f96f41996d612ae
João Victor <121417754+maliqto@users.noreply.github.com>
2023-07-27 03:31:40 -0300

    Add files via upload

---
+++ AULA 1 - LÓGICA AO PRÍMEIRO CÓDGIO/AULA C#  - 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AULA_C_____1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Este é meu primeiro programa em C#!");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
